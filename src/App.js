8933e1d3100dc6830aa661507410fd96d0902255
jon-cron <jonathanacm95@gmail.com>
2023-02-25 16:12:11 -0700

    push logic

--- src/App.js
+++ src/App.js
@@ -9,7 +9,53 @@ function App() {
   const [pickOne, setPickOne] = useState(null)
   const [pickTwo, setPickTwo] = useState(null)
   const [disabled, setDisabled] = useState(false)
+  const [wins, setWins] = useState(0)
 
+
+  const handleClick = (card) => {
+    if(!disabled) {
+      pickOne ? setPickTwo(card) : setPickOne(card)
+    }
+  }
+  const handleTurn = () => {
+    setPickOne(null)
+    setPickTwo(null)
+    setDisabled(false)
+  }
+  useEffect(() => {
+    let pickTimer;
+    if(pickOne && pickTwo){
+      if(pickOne.image === pickTwo.image){
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if(card.image === pickOne.image){
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
+        handleTurn();
+      } else {
+        setDisabled(true)
+        pickTimer = setTimeout(() => {
+          handleTurn();
+        }, 1000)
+      }
+    }
+    return () => {
+      clearTimeout(pickTimer)
+    }
+  }, [cards, pickOne, pickTwo])
+  useEffect(() => {
+    const checkWin = cards.filter((card) => !card.matched)
+    if(cards.length && checkWin.length < 1){
+      console.log('You win!!!')
+      setWins(wins + 1);
+      handleTurn()
+      setCards(shuffle)
+    }
+  }, [cards, wins])
   return (
     <>
 
@@ -21,8 +67,8 @@ function App() {
             <Card
               key={id}
               image={image}
-              selected={false}
-              onClick={() => {}}
+              selected={card === pickOne || card === pickTwo || matched}
+              onClick={() => handleClick(card)}
             />
           );
         })}
